819eff24645ac6ac16d371f3873cc55643aceb24
Magnus <abeikumagnus@gmail.com>
2026-05-16 20:30:35 +0100

    feat: initialize App component with header, hero section, and router outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './core/header/header';
+import { HeroSection } from './core/hero-section/hero-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, HeroSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
