360e81e3cfc22229d794da4c95abdc0b8fb8d309
Magnus <abeikumagnus@gmail.com>
2026-05-16 23:03:05 +0100

    feat: initialize application shell and implement feedback section component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './core/header/header';
 import { HeroSection } from './core/hero-section/hero-section';
 import { WhyChooseUs } from './core/why-choose-us/why-choose-us';
+import { Feedback } from './core/feedback/feedback';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, HeroSection, WhyChooseUs],
+  imports: [RouterOutlet, Header, HeroSection, WhyChooseUs, Feedback],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
