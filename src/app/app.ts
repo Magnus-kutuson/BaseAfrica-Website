407a380206591439d27eeac168d254154451f409
Magnus <abeikumagnus@gmail.com>
2026-05-16 23:12:24 +0100

    feat: implement main application layout and add core operational services section

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './core/header/header';
 import { HeroSection } from './core/hero-section/hero-section';
 import { WhyChooseUs } from './core/why-choose-us/why-choose-us';
 import { Feedback } from './core/feedback/feedback';
+import { Operations } from './core/operations/operations';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, HeroSection, WhyChooseUs, Feedback],
+  imports: [RouterOutlet, Header, HeroSection, Operations, WhyChooseUs, Feedback],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
