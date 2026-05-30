0db731ff880d835385909a2a645dde143c8c0c55
Magnus <abeikumagnus@gmail.com>
2026-05-30 19:29:36 +0100

    feat(projects): redesign projects page with detailed sections and visual layout

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from "../../core/header/header";
+import { Footer } from "../../core/footer/footer";
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
