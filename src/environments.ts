96155b7151c3761b5dc176ab7563e961a4a05f88
Magnus <abeikumagnus@gmail.com>
2026-06-16 12:35:02 +0100

    Stop tracking environment.ts

--- src/environments.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  emailjs: {
-    publicKey:  'WSvY6mRnhSapq7Mq1',
-    serviceId:  'service_bv07eyg',
-    templateId: 'template_5ff9dsr',
-  }
-};
