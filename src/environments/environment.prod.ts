72d7bf389869fb61df1d3930f24f8d2d104fa732
Magnus <abeikumagnus@gmail.com>
2026-06-21 15:39:48 +0100

    Add EmailJS credentials to environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+ production: true,
+  emailjs: {
+    publicKey: 'WSvY6mRnhSapq7Mq1',
+    serviceId: 'service_bv07eyg',
+    templateId: 'template_5ff9dsr',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+ production: false,
+  emailjs: {
+    publicKey: 'WSvY6mRnhSapq7Mq1',
+    serviceId: 'service_bv07eyg',
+    templateId: 'template_5ff9dsr',
+  }
+};
